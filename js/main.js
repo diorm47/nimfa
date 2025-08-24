0a5efe1786e1164c4a50465f3e0ab80ddb29d4df
diorm47 <dior47m@gmail.com>
2025-08-25 00:25:48 +0500

    checkbox fix

--- js/main.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll(".checkbox-item").forEach((item) => {
-  item.addEventListener("click", () => {
-    const area = item.querySelector(".checkbox-item__area");
-    area.classList.toggle("checkbox-item_selected");
-  });
-});
